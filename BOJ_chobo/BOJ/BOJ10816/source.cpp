a2680e7fda3cc43acb90d4a929b3f6db3d411135
LTH9898 <35323806+LTH9898@users.noreply.github.com>
2021-04-20 20:53:48 +0900

    4/20

--- BOJ_chobo/BOJ/BOJ10816/source.cpp
+++ BOJ_chobo/BOJ/BOJ10816/source.cpp
@@ -1,24 +1,102 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-
 using namespace std;
+
+int upperBound(vector<int> v, int value)
+{
+	int s = 0;
+	int e = v.size() - 1;
+
+	while (e > s)
+	{
+		int mid = (s + e) / 2;
+
+		if (v[mid] <= value)
+		{
+			s = mid + 1;
+		}
+		else
+		{
+			e = mid;
+		}
+	}
+
+	if (v[e] != value)
+	{
+		e -= -1;
+	}
+
+	return e;
+}
+
+int lowerBound(vector<int> v, int value)
+{
+	int s = 0;
+	int e = v.size() - 1;
+
+	while (e > s)
+	{
+		int mid = (s + e) / 2;
+
+		if (v[mid] < value)
+		{
+			s = mid + 1;
+		}
+		else
+		{
+			e = mid;
+		}
+	}
+
+	return e;
+
+}
+
+
+
+
 
 int main() {
 	int T;
 	int M;
-	vector<int> v(T);
-
 	
+	cin >> T;
 
+	vector<int> v;
+	int low;
+	int up;
+	int num;
+	int num2;
 	
-
 	for (int i = 0; i < T; i++)
 	{
-		cin >> 
+		cin >> num;
+		v.push_back(num);
 	}
-	
-	
 
+	sort(v.begin(), v.end());
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++)
+	{
+		int ans = 0;
+		cin >> num2;
+
+		low = lowerBound(v, num2);
+		up = upperBound(v, num2);
+
+		if (v[low] == num2)
+		{
+			ans = up - low;
+		}
+		else {
+			ans = 0;
+		}
+		cout << ans << ' ';
+	}
+
+	return 0;
 }
+

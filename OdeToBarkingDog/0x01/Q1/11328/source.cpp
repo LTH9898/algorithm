bd30c9501e9f8554fab03d8fdc85e75cb6150ce0
LTH9898 <35323806+LTH9898@users.noreply.github.com>
2021-04-22 14:55:15 +0900

    4/22

--- OdeToBarkingDog/0x01/Q1/11328/source.cpp
+++ OdeToBarkingDog/0x01/Q1/11328/source.cpp
@@ -1,40 +1,75 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <string.h>
 using namespace std;
 
 int main() {
-	vector<char> v1;
-	vector<char> v2;
+	int matchpoint = 0;
+	int N;
+	int alphamap[30];
 	string A;
 	string B;
-	int N;
 
 	cin >> N;
 
 	for (int i = 0; i < N; i++)
 	{
+		matchpoint = 0;
+		memset(alphamap, 0, sizeof(alphamap));
+		A = "";
+		B = "";
 		cin >> A;
 		cin >> B;
 
-		for (int j = 0; j < A.size(); j++)
+		if (A.length()!= B.length())
 		{
-			int find = 0;
-			for (int z = 0; z < B.size(); z++)
+			
+			cout << "Impossible!" << '\n';
+			continue;
+		}
+
+		for (int i = 0; i < A.length(); i++)
+		{
+			int temp = A[i] - 'a';
+			alphamap[temp]++;
+		}
+
+		for (int i = 0; i < B.length(); i++)
+		{
+			int temp = B[i] - 'a';
+			if (alphamap[temp] == 0)
 			{
-				if (A[j] == B[z]) {
-					find = 1;
-					break;
-				}
-			}
-			if (find == 1)
-				continue;
-			else {
-				cout << "impossible!";
+				alphamap[temp]++;
 				break;
 			}
-			
+			else
+			{
+				alphamap[temp]++;
+			}
 		}
+
 		
+		for (int i = 0; i < 30; i++)
+		{
+			if (alphamap[i] % 2 != 0)
+			{
+				matchpoint = 1;
+				break;
+			}
+		}
+
+		if (matchpoint == 1)
+		{
+			cout << "Impossible!" << '\n';
+		}
+		else if (matchpoint == 0)
+		{
+			cout << "Possible!" << '\n';
+		}
+	
 	}
+	
+	return 0;
 }
--- OdeToBarkingDog/0x01/Q1/Q2(withO(N))/source.cpp
+++ OdeToBarkingDog/0x01/Q1/Q2(withO(N))/source.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int arr[101] = { 0, };
+
+
+int func2(int *arr, int a)
+{
+	for (int i = 0; i < a; i++)
+	{
+		int temp;
+		int temp2;
+		cin >> temp;
+		temp2 = 100 - temp;
+		if (temp == temp2)
+		{
+			continue;
+		}
+		arr[temp] = 1;
+		if (arr[temp2] == 1)
+		{
+			return 1;
+			break;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return 0;
+}
+
 int main()
 {
-
+	int N;
+	cin >> N;
+	cout << func2(arr, N);
+	
 }
